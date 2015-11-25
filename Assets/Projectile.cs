49e5a3392335d5461a026f8e098d070b1e782055
Alan Huynh <artifion@gmail.com>
2015-11-24 22:44:46 -0800

    Initial commit

---
+++ Assets/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = rb.velocity*1.01f;
+	}
+	void Awake()
+	{
+		Destroy(this.gameObject, 5.0f);
+	}
+}
---
+++ Assets/ProjectileScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject prefab;
+	void Start () {
+		prefab = Resources.Load ("Projectile") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Vector3 position = new Vector3(Input.mousePosition.x, 5.0f ,Input.mousePosition.y);
+			position = Camera.main.ScreenToWorldPoint(position);
+			GameObject projectile = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+			projectile.transform.LookAt(position);
+
+			Rigidbody rb = projectile.GetComponent<Rigidbody>();
+			rb.velocity = projectile.transform.forward*50;
+			//rb.AddForce(position);
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "") {
+		
+		} else if (other.gameObject.name == "") {
+		
+		}
+	}
+}
